72652d5c07b4748710b5a11acde31773ac6473ce
w0832389 <W0824592@myscc.ca>
2023-11-27 16:21:46 -0500

    Add CoverType class and interface also update CoverType.cs

---
+++ LovepreetsBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,32 @@
+﻿using LovepreetsBooks.DataAccess.Repository.IRepository;
+using LovepreetsBooks.Models;
+using LovepreetsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LovepreetsBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            //us .NET LINQ to retrieve the first or default category object,
+            //thn pass the id as a generic entity which matches the category ID
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null) //save changes if not null 
+            {
+                objFromDb.Name = coverType.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
--- LovepreetsBooks.Models/CoverType.cs
+++ LovepreetsBooks.Models/CoverType.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace LovepreetBook.Model
+namespace LovepreetsBooks.Models
 {
     public class CoverType
     {
