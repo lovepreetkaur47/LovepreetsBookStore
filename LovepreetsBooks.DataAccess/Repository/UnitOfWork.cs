be78c508fc294201abd0be64c176dddf392f51d9
w0832389 <W0824592@myscc.ca>
2023-11-27 05:15:41 -0500

    add UnitOfWork and IUnitOfWork

---
+++ LovepreetsBooks.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using LovepreetsBooks.DataAccess.Repository.IRepository;
+using LovepreetsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LovepreetsBooks.DataAccess.Repository
+{
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext _db; //the using statement
+
+        public UnitOfWork(ApplicationDbContext db)   //constructor to use DI and inject in to the repositories
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()  //all changes will be saved when the save method is called at the 'parent' level
+        {
+            _db.SaveChanges();
+        }
+
+
+    }
+}
